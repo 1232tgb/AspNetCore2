6f00104caa3f7fd18875c2b3f310c403f9593ec3
1232tgb <48038393+1232tgb@users.noreply.github.com>
2019-10-13 23:52:41 -0300

    Commit Inicial
    Inclusão do projeto;
    inclusão da classe "Carro".

---
+++ AspNetCore1SLN/AspNetCore1/Models/Carro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore1.Models
+{
+    public class Carro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+    }
+}
